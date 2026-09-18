c18f458a1946fbed6a9774de582419e1dace212f
Miguel <migz.zgim@gmail.com>
2026-09-18 11:45:59 +0800

    Adds stub functions for unit tests and coverage

--- webapp.py
+++ webapp.py
@@ -6,6 +6,18 @@ from lib.properties import Properties
 webapp_properties = Properties('Properties.ini').webapp()
 app = Flask(__name__)
 
+def stub_1():
+    """
+    This is a stub function for the webapp test cases to work initially
+    """
+    return 1
+
+def stub_2():
+    """
+    This is a stub function to simulate missed coverage
+    """
+    return 2
+
 
 if __name__ == '__main__':
     app.run(host=webapp_properties['host'], port=webapp_properties['port'])
